efa3ed086e59562cc41142e79d854d11bac709e5
SylarCao <appledev014@sylars-MacBook-Pro.local>
2014-05-19 16:35:05 +0800

    a

--- cloud/main.js
+++ cloud/main.js
@@ -9,15 +9,14 @@ AV.Cloud.define("hello", function(request, response) {
                 var1.save(null, {
                                success: function(gameScore) {
                                // Execute any logic that should take place after the object is saved.
-//                               alert('New object created with objectId: ' + gameScore.id);
-                          response.success("success");
+                               alert('New object created with objectId: ' + gameScore.id);
                                },
                                error: function(gameScore, error) {
                                // Execute any logic that should take place if the save fails.
                                // error is a AV.Error with an error code and description.
-//                               alert('Failed to create new object, with error code: ' + error.description);
-                          response.success("fail");
+                               alert('Failed to create new object, with error code: ' + error.description);
                                }
+                               });
                 
                 
 //  response.success("Hello worlddddddd!");
