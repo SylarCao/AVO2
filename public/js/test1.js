6c70ba26ab4820a0dfeac352ef92d09b82b69f11
SylarCao <appledev014@sylars-MacBook-Pro.local>
2014-05-19 16:18:17 +0800

    test1

---
+++ public/js/test1.js
@@ -0,0 +1,9 @@
+
+
+AV.Cloud.define("test1", function(request, response) {
+                
+                var var1 = AV.Object.extend("CClass1");
+                
+                
+  response.success("test1");
+});
